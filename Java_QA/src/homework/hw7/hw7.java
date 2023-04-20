539edb5e2bea249ca295dec53bacbe3312d1f75c
DenisLavrenyuk <lavrenjuk9@gmail.com>
2023-04-21 02:55:47 +0300

    homework7-finished

--- Java_QA/src/homework/hw7/hw7.java
+++ Java_QA/src/homework/hw7/hw7.java
@@ -1,34 +1,34 @@
 package homework.hw7;
 
 import java.util.Arrays;
-import java.util.Scanner;
-
-import static homework.hw7.arrayMethods.addToArray;
 
 public class hw7 {
     public static void main(String[] args) {
-        int[] mass = new int[]{1, 2, 3, 4, 5};
-        System.out.println("Array: "+Arrays.toString(mass));
-        int number=100, position=1;
-//        arrayMethods.addToArray(mass, number);
-//        arrayMethods.addToArray(mass, number, position);
-        System.out.println("Array: "+Arrays.toString(addToArray(mass,number)));
-        System.out.println("Array: "+Arrays.toString(addToArray(mass,number,position)));
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("enter array element");
-//        if (scanner.hasNextInt()) {
-//            number = scanner.nextInt();
-//        }
-//        else {System.out.println("ERROR: wrong command");
-//            System.exit(0);
-//        }
-//        System.out.println("enter position");
-//        if (scanner.hasNextInt()) {
-//            position = scanner.nextInt();
-//        }
-//        else {System.out.println("ERROR: wrong command");
-//            System.exit(0);
-//        }
+        int[] massive = new int[]{1, 2, 3, 4, 5};
+        System.out.println("Initial Array: " + Arrays.toString(massive));
 
+        int number = 100, position = 2;
+        System.out.println("Number: "+number+" Position: "+position+"\n");
+
+      addToArray(new int[]{1, 2, 3, 4, 5}, number);
+      addToArray(new int[]{1, 2, 3, 4, 5}, number, position);
+    }
+    public static void addToArray(int[]massive,int number) {
+            for (int t = massive.length - 1; t > 0; t--) {
+                massive[t] = massive[t - 1];
+            }
+            massive[0] = number;
+            System.out.println("Default Array: " + Arrays.toString(massive));
+        }
+    public static void addToArray(int[]massive,int number, int position){
+        if (position>=0 && position<massive.length) {
+            for (int t = massive.length - 1; t > position; t--) {
+                massive[t] = massive[t - 1];
+            }
+            massive[position] = number;
+            System.out.println("Positioned Array: " + Arrays.toString(massive));
+        }
+        else System.out.println("Error: position is out of array");
     }
 }
+
