09212d2bc8e8791cfba46975f9b02616a83a9113
DenisLavrenyuk <lavrenjuk9@gmail.com>
2023-04-27 14:31:40 +0300

    homework10-check added

--- Java_QA/src/homework/hw10/hw10.java
+++ Java_QA/src/homework/hw10/hw10.java
@@ -4,19 +4,30 @@ import java.util.*;
 
 public class hw10 {
     public static void main(String[] args) {
-        String str="butter, butter,.. milk,, milk,, meat, ham ";
-        magicString(str);
+        String str=" butter, butter,.. milk,, MILK,, meat, cheese ";
+        checkString(str);
     }
 
-    private static void magicString(String str) {
-        String[] words=str.replaceAll("[,.]", "").split(" ");
-        Set<String> set= new HashSet<>();
-        Collections.addAll(set, words);
-//        Arrays.asList(words) - analog
-//        for (String word:words) {
-//            set.add(word);}
-        for (String word:set) {
-        System.out.println(word.replaceFirst(word.substring(0,1), word.substring(0,1).toUpperCase()));
+    public static void checkString(String str) {
+
+        boolean check = str.startsWith(" ");
+        if (!check) {
+            magicString(str);
+        } else {
+            str = str.replaceFirst(" ", "");
+            magicString(str);
         }
     }
-}
+    public static void magicString(String str) {
+            String[] words=str.replaceAll("[,.]", "").toLowerCase().split(" ");
+            Set<String> set= new HashSet<>();
+            Collections.addAll(set, words);
+//                    Arrays.asList(words) - analog
+//        for (String word:words) {
+//            set.add(word);}
+            for (String word:set) {
+                System.out.println(word.replaceFirst(word.substring(0,1), word.substring(0,1).toUpperCase()));
+            }
+        }
+    }
+
