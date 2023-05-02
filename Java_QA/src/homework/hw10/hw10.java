7ab668ad88a8f5a54b1ff5e7dc7a58b456154255
DenisLavrenyuk <lavrenjuk9@gmail.com>
2023-05-02 14:29:59 +0300

    homework10-refactored

--- Java_QA/src/homework/hw10/hw10.java
+++ Java_QA/src/homework/hw10/hw10.java
@@ -4,27 +4,11 @@ import java.util.*;
 
 public class hw10 {
     public static void main(String[] args) {
-        String str=" butter, butter,.. milk,, MILK,, meat, cheese ";
-        checkString(str);
+        magicString(" butter, butter,  milk, MILK,    meat, cheese.");
     }
 
-    public static void checkString(String str) {
-
-        boolean check = str.startsWith(" ");
-        if (!check) {
-            magicString(str);
-        } else {
-            str = str.replaceFirst(" ", "");
-            magicString(str);
-        }
-    }
     public static void magicString(String str) {
-            String[] words=str.replaceAll("[,.]", "").toLowerCase().split(" ");
-            Set<String> set= new HashSet<>();
-            Collections.addAll(set, words);
-//                    Arrays.asList(words) - analog
-//        for (String word:words) {
-//            set.add(word);}
+        Set<String> set= new HashSet<>(Arrays.asList(str.toLowerCase().replaceAll("[ .]", "").split(",")));
             for (String word:set) {
                 System.out.println(word.replaceFirst(word.substring(0,1), word.substring(0,1).toUpperCase()));
             }
