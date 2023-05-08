7295a124aa633917b9200a47a52a8938d3533bc7
DenisLavrenyuk <lavrenjuk9@gmail.com>
2023-05-08 13:19:44 +0300

    test ssh upload

--- Java_QA/src/main/java/lessons/Main.java
+++ Java_QA/src/main/java/lessons/Main.java
@@ -2,9 +2,9 @@ package lessons;
 
 public class Main {
     public static void main(String[] args) {
-        int ABC=15, T=20, K=0;
+        int ABC=150, T=200, K=0;
         K=ABC+T;
         System.out.println(K);
-
+        //test ssh upload
     }
 }
